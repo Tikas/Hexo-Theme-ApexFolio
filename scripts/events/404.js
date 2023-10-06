814841ee7fd9f1a302537ed2e06912283b2142c4
Tikas <Tikas@qq.com>
2023-10-06 11:43:26 +0800

    func(404): 增加简单的 404 页面

---
+++ scripts/events/404.js
@@ -0,0 +1,13 @@
+'use strict'
+
+hexo.extend.generator.register('404', function (locals) {
+  if (!hexo.theme.config.error_404.enable) return
+  return {
+    path: '404.html',
+    layout: ['page'],
+    data: {
+      type: '404',
+      sitemap: false
+    }
+  }
+})
